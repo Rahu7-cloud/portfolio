0aa36b6e87ee3b6b2b9d84acff54851d538e2eb9
Rahu7-cloud <patnaik.rahul2503@gmail.com>
2023-07-05 15:46:17 +0530

    Added hamburger menu to landing page

--- app.js
+++ app.js
@@ -40,3 +40,86 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+//hamburger menu animation
+const tl = gsap.timeline({ paused: true });
+let path = document.querySelector("path");
+let spanBefore = CSSRulePlugin.getRule("#hamburger span:before");
+
+gsap.set(spanBefore, { background: "#ffe29a" });
+gsap.set(".menu", { visibility: "hidden" });
+
+function revealMenu() {
+  revealMenuItems();
+
+  const hamburger = document.getElementById("hamburger");
+  const toggleBtn = document.getElementById("toggle-btn");
+
+  toggleBtn.onclick = function (e) {
+    hamburger.classList.toggle("active");
+    tl.reversed(!tl.reversed());
+  };
+
+  revealMenu();
+
+  function revealMenuItems() {
+    const start = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+    const end = "M0,1005S175,995,500,995s500,5,500,5V0H0Z";
+
+    const power2 = "power2.inOut";
+    const power4 = "power4.inOut";
+
+    tl.to("#hamburger", 1.25, {
+      marginTop: "-5px",
+      x: -40,
+      y: 40,
+      ease: power4,
+    });
+
+    tl.to(
+      "#hamburger span",
+      1,
+      {
+        background: "#862b0d",
+        ease: power2,
+      },
+      "<"
+    );
+
+    tl.to(
+      ".btn .btn-outline",
+      1.25,
+      {
+        x: -40,
+        y: 40,
+        width: "140px",
+        height: "140px",
+        border: "1px solid #862b0d",
+        ease: power4,
+      },
+      "<"
+    );
+
+    tl.to(
+      path,
+      0.8,
+      {
+        attr: {
+          d: start,
+        },
+        ease: power2.easeIn,
+      },
+      "<"
+    ).to(
+      path,
+      0.8,
+      {
+        attr: {
+          d: end,
+        },
+        ease: power2.easeIn,
+      },
+      "0.5"
+    );
+  }
+}
